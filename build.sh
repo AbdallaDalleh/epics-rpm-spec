bd71793dec1c717d0521ecf691f5e8ffc4fd30ac
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2025-12-29 15:45:04 +0300

    build.sh: added individual calls to build_rpms.

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash
 
 set -e
-
-repos="seq autosave caputRecorder iocStats ether-ip sscan calc ipac asyn scaler stream-device modbus busy std motor mca support area-detector-core dxp area-detector-simulation area-detector-genicam area-detector-aravis area-detector-pylon area-detector-pilatus"
 
 function build_rpm() {
 	name="$1"
@@ -14,7 +12,8 @@ function build_rpm() {
 	version=${major}.${minor}
 	[[ -z "$build" ]] && build=0
 
-	rpmbuild -ba --define "_version ${version}" --define "build_number ${build}" SPECS/${name}.spec
+	rpmbuild -ba --define "_version ${version}" --define "build_number ${build}" SPECS/${name}.spec # 2&>1 /dev/null
+ 	sudo yum -y install $(ls -tr RPMS/x86_64/"$name"-"$version"* | grep -v debug | sort -u | tail -n-1)
 }
 
 [[ "$#" -ne 1 && "$#" -ne 2 ]] && {
@@ -25,18 +24,30 @@ function build_rpm() {
 }
 
 if [[ "$1" == "all" ]]; then
-	# list=$(ls -d -1 SOURCES/*/ | xargs -I% basename %)
-	list="$repos";
-	for module in $list; do
-		module=$(ls -t -1 SOURCES/*$module*.tar.gz | head -n1 | xargs -I% basename %)
-		mod_version=${module##*-}
-		mod_name=$(echo $module | sed "s/-$mod_version//g")
-		echo $mod_name - $mod_version
-		build_rpm $mod_name $mod_version
-		sudo yum -y install $(ls -1 RPMS/x86_64/"$mod_name"* | grep -v debug)
-	done
+    build_rpm sequencer 2.2.9
+    build_rpm autosave 6.0.0
+    build_rpm iocStats 3.2.0
+    build_rpm ether-ip 3.3.0
+    build_rpm sscan 2.11.6
+    build_rpm calc 3.7.5
+    build_rpm asyn 4.45.0
+    build_rpm scaler 4.1.0
+    build_rpm stream-device 2.8.26
+    build_rpm modbus 3.4.0
+    build_rpm busy 1.7.4
+    build_rpm std 3.6.4
+    build_rpm motor 7.3.1
+    build_rpm mca 7.10.0
+    build_rpm area-detector-support 1.10.0
+    build_rpm area-detector-core 3.14.0
+    build_rpm dxp 6.1.0
+    build_rpm area-detector-simulation 2.11.0
+    build_rpm area-detector-genicam 1.10.0
+    build_rpm area-detector-aravis 2.3.0
+    build_rpm area-detector-pylon 0.0.1
+    build_rpm area-detector-pilatus 2.9.0
+    build_rpm area-detector-spinnaker 3.3.0
 else
-	build_rpm "$1" "$2"
-	sudo yum -y install $(ls -1 RPMS/x86_64/"$1"* | grep -v debug)
+    build_rpm "$1" "$2"
 fi
 
